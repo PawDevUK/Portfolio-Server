307ac68bcd0b725aa3346c168e3e1ee70d54ae39
gitcrackerUK <pfsiwek@gmail.com>
2021-03-06 10:47:09 +0000

    added tictactoe routes for "get users"&"add user"

--- routes/Tictactoe/Tictactoe.js
+++ routes/Tictactoe/Tictactoe.js
@@ -4,5 +4,12 @@ router.route('/').get((req, res) => {
     res.send('Tictactoe')
 })
 
+router.route('/users').get((req, res) => {
+    res.send({ users: ["Pawel", "Tony"] })
+})
+
+router.route('/addUser').post((req, res) => {
+    res.send(req.body)
+})
 
 module.exports = router
