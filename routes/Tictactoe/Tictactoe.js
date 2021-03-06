de71a71de3b1949f98bb08f1aea514bff70ac932
gitcrackerUK <pfsiwek@gmail.com>
2021-03-06 14:36:45 +0000

    imported users schema and added routes

--- routes/Tictactoe/Tictactoe.js
+++ routes/Tictactoe/Tictactoe.js
@@ -1,15 +1,34 @@
 const router = require('express').Router()
+const Users = require('./models/users.model.js')
 
 router.route('/').get((req, res) => {
     res.send('Tictactoe')
 })
 
 router.route('/users').get((req, res) => {
-    res.send({ users: ["Pawel", "Tony"] })
+    Users.find()
+        .then((users) => res.json(users))
 })
 
-router.route('/addUser').post((req, res) => {
-    res.send(req.body)
+router.route('/users/addUser').post((req, res) => {
+    const user = req.body.user
+    const newUser = new Users({ user })
+    newUser.save()
+        .then(() => res.json('User added to available/active players'))
+        .catch(err => {
+            res.status(400).json(`Error is ${err}`)
+
+        }
+        )
+})
+
+router.route('/users/deleteUser/:id').delete((req, res) => {
+    Users.findByIdAndDelete(req.params.id)
+        .then(() => res.json('User removed from active players list!! '))
+        .catch(err => {
+            res.status(400).json(`Error is ${err}`)
+        }
+        )
 })
 
 module.exports = router
