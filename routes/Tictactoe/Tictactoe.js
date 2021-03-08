8eaa533df6d1c28b706f73ce8f2b76e1011cc6e8
gitcrackerUK <pfsiwek@gmail.com>
2021-03-08 11:44:40 +0000

    added function to create id in userobject

--- routes/Tictactoe/Tictactoe.js
+++ routes/Tictactoe/Tictactoe.js
@@ -1,25 +1,33 @@
 const router = require('express').Router()
 const Users = require('./models/users.model.js')
 
+function addID(arr) {
+    const arrWithId = []
+    for (let i = 0; i < arr.length; i++) {
+        arrWithId.push({
+            id: arr[i]._id,
+            ...arr[i]._doc,
+        })
+    }
+    return arrWithId
+}
+
 router.route('/').get((req, res) => {
     res.send('Tictactoe')
 })
 
 router.route('/users').get((req, res) => {
     Users.find()
-        .then((users) => res.json(users))
+        .then((users) => res.json(addID(users)))
 })
 
 router.route('/users/addUser').post((req, res) => {
-    const user = req.body.user
-    const newUser = new Users({ user })
+    const newUser = new Users(req.body)
     newUser.save()
         .then(() => res.json('User added to available/active players'))
         .catch(err => {
             res.status(400).json(`Error is ${err}`)
-
-        }
-        )
+        })
 })
 
 router.route('/users/deleteUser/:id').delete((req, res) => {
