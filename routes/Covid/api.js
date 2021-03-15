276fab568076a1f83ca2b93ca29b5300b2c8ed66
gitcrackerUK <pfsiwek@gmail.com>
2021-03-15 15:08:33 +0000

    api update

--- routes/Covid/api.js
+++ routes/Covid/api.js
@@ -6,12 +6,13 @@ const options = {
     method: 'GET',
     url: process.env.RAPID_NEWS_URL,
     params: {
-        q: 'Covid-19',
+        q: 'Covid',
         lang: 'en',
         sort_by: 'relevancy',
-        from: '2021/03/01',
-        to: '2021/03/13',
+        from: '2020/03/01',
+        search_in: 'title',
         country: 'UK',
+        page: '1',
         media: 'True',
     },
     headers: {
@@ -28,24 +29,25 @@ const GetNewsData = axios
     .catch(function (error) {
         return error;
     });
+
 // <----- news
 
-// ---- Global Data
-const GetGlobalData = axios.create({
-    baseURL: process.env.RAPID_GLOBAL_DATA_URL,
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': process.env.RAPID_GLOBAL_DATA_KEY,
-        'x-rapidapi-host': process.env.RAPID_GLOBAL_HOST_URL,
-        useQueryString: true,
-    },
-});
-const Global = async function (route) {
-    await GetGlobalData(`./${route}`)
-        .then((res) => console.log(JSON.stringify(res.data)))
-        .catch((err) => console.log(err));
-};
-Global('uk');
+// // ---- Global Data
+// const GetGlobalData = axios.create({
+//     baseURL: process.env.RAPID_GLOBAL_DATA_URL,
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-key': process.env.RAPID_GLOBAL_DATA_KEY,
+//         'x-rapidapi-host': process.env.RAPID_GLOBAL_HOST_URL,
+//         useQueryString: true,
+//     },
+// });
+// const Global = async function (route) {
+//     await GetGlobalData(`./${route}`)
+//         .then((res) => res)
+//         .catch((err) => console.log(err));
+// };
+
 // <--- Global Data
 module.exports = GetNewsData;
-module.exports = GetGlobalData;
+// module.exports = GetGlobalData;
