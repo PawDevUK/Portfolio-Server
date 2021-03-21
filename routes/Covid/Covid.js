547b94a7239c1fa39baa7f3c0619a9a540020401
GitcrackerUK <pfsiwek@gmail.com>
2021-03-21 17:17:58 +0000

    added   CheckStoreStatusAndSave fot route /GB

--- routes/Covid/Covid.js
+++ routes/Covid/Covid.js
@@ -3,18 +3,15 @@ const GetGlobalData = require('./API/GlobalData');
 const GetNewsData = require('./API/GlobalNews');
 const router = require('express').Router();
 const store_news = require('./Store/news.json');
-const store_GB_data = require('./Store/news.json');
+const store_GB_data = require('./Store/data_GB.json');
 
 router.route('/newsUK').get((req, res) => {
-    CheckStoreStatusAndSave(store_news, GetNewsData);
+    CheckStoreStatusAndSave(store_news, GetNewsData, 'news');
     res.send(store_news.articles).status(200);
 });
 
 router.route('/GB').get((req, res) => {
-    (async () => {
-        const data = await GetGlobalData('GB');
-    })();
-
+    CheckStoreStatusAndSave(store_GB_data, GetGlobalData('GB'), 'data_GB');
     res.send('stats').status(200);
 });
 
