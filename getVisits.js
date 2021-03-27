6f8d8d9c80070ebc0a18e58e6bcb3bec3ce01113
GitcrackerUK <pfsiwek@gmail.com>
2021-03-27 23:38:51 +0000

    added cloud function

---
+++ getVisits.js
@@ -0,0 +1,13 @@
+const
+
+const getVisitors = (req, res) => {
+    const method = req.method;
+    try {
+        if (method === 'GET') {
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getVisitors = getVisitors;
