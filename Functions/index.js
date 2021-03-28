9b84987d8f3bc85f9f29196ab504f4fbb38c0d7e
GitcrackerUK <pfsiwek@gmail.com>
2021-03-28 20:32:20 +0100

    removed mongoose  and added mongodb

--- Functions/index.js
+++ Functions/index.js
@@ -1,41 +1,12 @@
 const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+
+console.log(mongodb);
 
 const getVisitors = async (req, res) => {
-    const Schema = mongoose.Schema;
-    const visitorsSchema = new Schema(
-        {
-            counter: {
-                type: Number,
-                required: true,
-            },
-        },
-        {
-            timestamps: true,
-        }
-    );
-
-    const visitor = mongoose.model('Visitor', visitorsSchema);
-
     const method = req.method;
 
     if (method === 'GET') {
-        mongoose.connect(VISITORS_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        const connection = mongoose.connection;
-
-        connection.once('open', () => {
-            console.log('MongoDB database connection established successfully!!');
-        });
-
-        await visitor.find().then((counter) => {
-            const total = counter[counter.length - 1].counter;
-            res.send({ totalVisits: total });
-        });
-        mongoose.connection.close(() => {
-            console.log('Connection closed');
-        });
     }
 };
 
