3d4b1c581b066141c4feb427e8af0e279c068d2a
GitcrackerUK <pfsiwek@gmail.com>
2022-09-05 13:11:40 +0100

    added local host 3000 to cors

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const ChatBot = require('./routes/ChatBot/ChatBot.js');
 const Covid = require('./routes/Covid/Covid.js');
 const express = require('express');
 const cors = require('cors');
+const Register = require('./routes/RegisterLogin/Register.js')
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
 
 const Port = process.env.PORT || 8080;
 
@@ -18,11 +21,13 @@ const Port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hi there, this is base URL');
 });
+
 app.use('/covid', Covid);
 app.use('/chatBot', ChatBot);
 app.use('/visitor', VisitorRoute);
 app.use('/tictactoe', Tictactoe);
 app.use('/contactForm', ContactFormRoute);
+app.use('/register', Register)
 
 app.listen(Port, () => {
     console.log(`Example app listening at http://localhost:${Port}`);
