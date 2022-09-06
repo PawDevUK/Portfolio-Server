84103a81065151d22d606ebdc4abc5cce275b9a6
GitcrackerUK <pfsiwek@gmail.com>
2022-09-07 00:51:40 +0100

    imported moment js  and replaced new date methods

--- routes/WorkTracker/Tracker.js
+++ routes/WorkTracker/Tracker.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 
 const rates = {
     currency : 'GBP',
@@ -54,8 +55,6 @@ function checkWeekDay(year,month,day){
         weekday: 'long',
       })
 
-    return weekDay
-}
 function extractDateFromString(str){
     let date=[];
     date.push(parseInt(str.slice(0,2)))
@@ -63,34 +62,60 @@ function extractDateFromString(str){
 
     return date
 }
+
 function returnDate(...args){
     let payload = [];
-
     args.forEach((i)=>{
         if(typeof i === 'string' && args.length === 1){
             payload = extractDateFromString(args[0])
         }else if (typeof i === 'string' && args.length >=2 ){
             payload.push(parseInt(i))
-        }else if (typeof i === 'number' && args.length >= 2){
+        }else if (typeof i === 'number'){
             payload.push(i)
         }
     })
-    
-    return new Date(payload[1],payload[0])
+    return moment([payload[1],payload[0]-1])
 }
-// date format month/year
-function createMonth(date){
-    let intDate = extractDateFromString(date)
-    let passedDate = new Date(intDate[1],intDate[0],0)
-    let days = passedDate.getDate()
-    let monthName = passedDate.toLocaleString('default',{month:'long'})
-    let calendar = [monthName];
-    console.log(days)
 
+function getMonthName(m){
+    return  moment().month(m).format('MMMM')
+}
+
+function getNameOfWeekDay(payload,i){
+    const a =  moment(payload).date(i)
+    return moment(a).format('dddd')
+}
+
+function checkIN(arr,day){
+    let IN = false;
+    arr.forEach((i)=>{
+        if(i===day){
+            IN = true
+        }
+    })
+    return IN
+}
+
+// date format month/year
+function createMonth(...args){
+    const DateArg = returnDate(...args)
+    const month = moment(DateArg).month()+1
+    const year = moment(DateArg).year()
+    const monthName = getMonthName(month)
+    const days = moment().daysInMonth(month)
+    let calendar = { 
+        name:monthName,
+        month,
+        year,
+        numberOfDays:days,
+        days:[]   
+    };
+    const arr = [1,2,3,4,5,7,8,9,12,23];
     for( let i = 1 ; i <= days; i++){
-        calendar.push({
+        calendar.days.push({
             day:i,
-            weekDay:checkWeekDay(intDate[1],intDate[0],i)
+            in:checkIN(arr, i),
+            weekDay:getNameOfWeekDay(DateArg,i),
         })
     }
     
