2fc9f33467608323acb476f1842b3dc2a08c4b0c
GitcrackerUK <pfsiwek@gmail.com>
2022-09-10 02:40:56 +0100

    added function calcEarnedForDay

--- routes/WorkTracker/Tracker.js
+++ routes/WorkTracker/Tracker.js
@@ -103,6 +103,34 @@ function countDays(obj){
     return counter
 }
 
+function calcEarnedForDay(day){
+    const days = {
+        1:'weekday',
+        2:'friday',
+        3:'sat',
+        4:'sun'
+    }
+    let payload;
+    if(day===days[1]){
+       const dayH = 5 * 16.75;
+       const nightH = 4.25 * 20.93;
+       payload = dayH + nightH
+    }else if(day===days[2]){
+        const dayH = 5 * 16.75;
+        const nightH = 2 * 20.93;
+        const weekendH = 2.25 * 22.27;
+        payload = dayH + nightH + weekendH 
+    }else if(day===days[3]){
+        const weekendH = 9.25 * 22.27;
+        payload = weekendH
+    }else if(day===days[4]){
+        const weekendH = 7 * 22.27;
+        const nightH = 2.25 * 20.93;
+        payload = weekendH + nightH
+    }
+    return payload
+}
+
 // date format month/year
 function createMonth(rota){
     const {OffDays,date} = rota;
@@ -125,10 +153,10 @@ function createMonth(rota){
         IN_sat:null,
         IN_sun:null,
         pay_for_day:{
-            weekDay:null,
-            friday:null,
-            sat:null,
-            sun:null
+            weekDay:calcEarnedForDay('weekday'),
+            friday:calcEarnedForDay('friday'),
+            sat:calcEarnedForDay('sat'),
+            sun:calcEarnedForDay('sun')
         },
         rates:{
                 currency :'GBP',
