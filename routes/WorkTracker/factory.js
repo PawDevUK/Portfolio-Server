98d2e329437af90422b501e2749c14dbf3f53b60
GitcrackerUK <pfsiwek@gmail.com>
2022-10-04 15:29:46 +0100

    changed import

--- routes/WorkTracker/factory.js
+++ routes/WorkTracker/factory.js
@@ -1,4 +1,4 @@
-const weekCombinations = require('./store').weekCombinations;
+const {weekCombinations} = require('./store');
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
 const fs = require('fs');
