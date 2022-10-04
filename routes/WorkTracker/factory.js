0cc05869a00ab0ed1461832fa08d586f95884297
GitcrackerUK <pfsiwek@gmail.com>
2022-10-04 15:21:03 +0100

    moved down the file as not used at the moment

--- routes/WorkTracker/factory.js
+++ routes/WorkTracker/factory.js
@@ -190,18 +190,6 @@ function getFinishBasic(start_Time){
 }
 
 
-function checkPercentageFrom(main,fraction){
-    let percent;
-    let stPerce;
-
-    let mainDivided = main/100
-
-    percent = fraction / mainDivided
-
-    stPerce = percent.toString().slice(0,4)
-
-    return `${fraction} is ${stPerce} of ${main}`
-}
 
 function calcPercent(basic,extraRate){
     let extra = 0 
@@ -324,6 +312,18 @@ function writeToResults(payload){
                 }
         }
     )
+}
+function checkPercentageFrom(main,fraction){
+    let percent;
+    let stPerce;
+
+    let mainDivided = main/100
+
+    percent = fraction / mainDivided
+
+    stPerce = percent.toString().slice(0,4)
+
+    return `${fraction} is ${stPerce} of ${main}`
 }
 
 module.exports = {
