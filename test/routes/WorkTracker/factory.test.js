a7ec5d1da76a7b1e8a21b96a9cb0fb46752c7de9
GitcrackerUK <pfsiwek@gmail.com>
2022-10-04 21:35:42 +0100

    changed import

--- test/routes/WorkTracker/factory.test.js
+++ test/routes/WorkTracker/factory.test.js
@@ -1,4 +1,4 @@
-const calcPercent = require('../../../routes/WorkTracker/factory.js').calcPercent
+const {calcPercent} = require('../../../routes/WorkTracker/factory.js')
 
 test('Should return 1.5', () => { expect(calcPercent(1,50)).toBe(1.5)})
 test('Should return 12.5', () => { expect(calcPercent(10,25)).toBe(12.5)})
