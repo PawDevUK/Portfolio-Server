22f6b643035c34d432a2dda8a39af00f82805e0d
GitcrackerUK <pfsiwek@gmail.com>
2022-10-11 14:00:37 +0100

    Commented out not used components.  Added helper functions, variable, and monday hour generator.

--- routes/WorkTracker/factory.js
+++ routes/WorkTracker/factory.js
@@ -125,15 +125,54 @@ function countDays(obj){
 //** @function 
 /** @name calcEarnedForDay at the moment this function calculate earnings for day only between 17:00 and 02:15
 */
-function calcEarnedForDay(rates,calc,reduceFloat,start_Time){
+function calcEarnedForDay(
+    // rates,
+    // calc,
+    // reduceFloat,
+    getFinishBasic,
+    start_Time,
+    finish_Time
+    ){
 
+    // moment methods used in this function:
+    //  - a.from(b)
+    //  - [before].isBefore([after]) true
+    //  - [after].isAfter([before])  true
+    //  - a.diff(b,'minutes')
+
+
+    function returnTime(date,h,m){
+        return moment(date).hour(h).minute(m);
+    }
+    
+    function checkDurationTime(start_Time,finish_Time){
+        return moment(start_Time).from(finish_Time)
+    }
+
+    const startTime = moment(start_Time);
+    let finishBasicTime = getFinishBasic(start_Time);
+    let finishTime = '';
+    let overtimeTotal = '';
+    if(finish_Time){
+        finishTime = moment(finish_Time);
+        overtimeTotal = checkDurationTime(startTime, finish_Time) - returnTime(startTime,9,15) /// initial 
+    }
+    
+    
     // calculate from start to plus 9.25h
     // create break points for different rates
     //  week day monday-friday 06:00 - 22:00
 
-    const dayRateStart = moment().hour(06).minute(00);
-    const NighRateStart = moment().hour(22).minute(00);
-    const WeekendRateStart = moment().hour(00).minute(00);
+    const dayRateTime = returnTime(startTime,06,00);
+    const nightRateTime = returnTime(startTime,22,00);
+    const weekendRateTime = returnTime(startTime,00,00);
+
+    console.log({
+        dayRateTime,
+        nightRateTime,
+        weekendRateTime,
+        finishBasicTime,
+    });
 
     //  week night monday 00:00 - 06:00
     //  week night tuesday-thursday 22:00 - 06:00
@@ -144,34 +183,93 @@ function calcEarnedForDay(rates,calc,reduceFloat,start_Time){
     //  weekend night sat 22:00 - 06:00
     //  weekend night sun 22:00 - 00:00
 
-    const { basic, nights, weekends } = rates;
+    // const { basic, nights, weekends } = rates;
 
     let payload = {};
-    
-    function weekDay(){
-        const dayH = 5 * basic; //from 17:00 till 22:00 is 5h 
-        const nightH = 4.25 * calc(basic,nights.percent); // from 22:00 till finish 02:15 is 4h 15min
-        return dayH + nightH;
-    }
-    function friday(){
-        const dayH = 5 * basic; //from 17:00 till 22:00 is 5h 
-        const nightH = 2 * calc(basic,nights.percent); 
-        const weekendH = 2.25 * calc(basic,weekends.percent);
-        return dayH + nightH + weekendH;
-    }
-    function sat(){
-        return 9.25 * calc(basic,weekends.percent);
-    }
-    function sun(){
-        const weekendH = 7 * calc(basic,weekends.percent);
-        const nightH = 2.25 * calc(basic,nights.percent); // week night from 00:00
-        return weekendH + nightH;
-    }
 
-    payload['weekDay'] = reduceFloat(weekDay());
-    payload['friday'] = reduceFloat(friday());
-    payload['sat'] = reduceFloat(sat());
-    payload['sun'] = reduceFloat(sun());
+    function monday(start_Time){
+        const times = {
+            nightHours:null,
+            dayHours:null,
+            overtime:null
+        }
+        if ( start_Time.isSameOrAfter(weekendRateTime) && start_Time.isBefore(dayRateTime) ){
+            times.nightHours = dayRateTime.diff(start_Time,'minutes') / 60;
+            times.dayHours = finishBasicTime.diff(dayRateTime,'minutes') / 60;
+        }
+        else if (start_Time.isSameOrAfter(dayRateTime) && finishBasicTime.isBefore(nightRateTime)){
+            times.dayHours = finishBasicTime.diff(start_Time,'minutes') / 60;
+        }
+        else if (start_Time.isAfter(dayRateTime) && start_Time.isBefore(nightRateTime) && finishBasicTime.isSameOrAfter(nightRateTime) && finishBasicTime.isBefore(moment(dayRateTime).add(1,'day'))){
+            times.dayHours = nightRateTime.diff(start_Time,'minutes') / 60;
+            times.nightHours = finishBasicTime.diff(nightRateTime,'minutes') / 60;
+        }
+        else if ( start_Time.isBefore(nightRateTime) && finishBasicTime.isSameOrAfter(moment(dayRateTime).add(1,'day'))){
+            let nextDayHours = 0
+            times.dayHours = nightRateTime.diff(start_Time,'minutes') / 60;
+            nextDayHours = finishBasicTime.diff(moment(dayRateTime).add(1,'day'),'minutes') / 60;
+            times.dayHours += nextDayHours
+            times.nightHours = finishBasicTime.diff(nightRateTime,'minutes') / 60 - nextDayHours;
+        }
+        else if(start_Time.isSameOrAfter(nightRateTime)) {
+            times.nightHours = moment(dayRateTime).add(1,'day').diff(start_Time,'minutes') / 60;
+            times.dayHours = finishBasicTime.diff(moment(dayRateTime).add(1,'day'),'minutes') / 60;
+        }
+
+        return times
+    }
+    
+    function weekDay(start_Time){
+        const times = {
+            nightHours:null,
+            dayHours:null,
+            overtime:null
+        }
+        if(start_Time.isSameOrAfter(returnTime(start_Time,00,00)) && start_Time.isBefore(dayRateTime) && finishBasicTime.isAfter(dayRateTime)){
+            console.log('---- > 1');
+        }if(start_Time.isSameOrAfter(dayRateTime) && finishBasicTime.isSameOrBefore(nightRateTime)){
+            console.log('---- > 2');
+        }if(start_Time.isAfter(dayRateTime) && finishBasicTime.isAfter(nightRateTime)){
+            console.log('---- > 3');
+        }
+        // const dayH = 5 * basic; //from 17:00 till 22:00 is 5h 
+        // const nightH = 4.25 * calc(basic,nights.percent); // from 22:00 till finish 02:15 is 4h 15min
+        // return dayH + nightH;
+        return times
+    }
+    // function friday(start_Time){
+    //     const dayH = 5 * basic; //from 17:00 till 22:00 is 5h 
+    //     const nightH = 2 * calc(basic,nights.percent); 
+    //     const weekendH = 2.25 * calc(basic,weekends.percent);
+    //     return dayH + nightH + weekendH;
+    // }
+    // function sat(start_Time){
+    //     return 9.25 * calc(basic,weekends.percent);
+    // }
+    // function sun(start_Time){
+    //     const weekendH = 7 * calc(basic,weekends.percent);
+    //     const nightH = 2.25 * calc(basic,nights.percent); // week night from 00:00
+    //     return weekendH + nightH;
+    // }
+
+    if(moment(startTime).format('dddd')==='Monday'){
+      payload = monday(startTime);
+    }
+    else if(moment(startTime).format('dddd')==='Tuesday' || moment(startTime).format('dddd')==='Wednesday' || moment(startTime).format('dddd')==='Thursday'){
+      payload = weekDay(startTime);
+    }
+    // else if(moment(startTime).format('dddd')==='Friday'){
+    //   payload = friday(startTime);
+    // }else if(moment(startTime).format('dddd')==='Saturday'){
+    //   payload = saturday(startTime);
+    // }else if(moment(startTime).format('dddd')==='Saturday'){
+    //   payload = sunday(startTime);
+    // }
+
+    // payload['weekDay'] = reduceFloat(weekDay());
+    // payload['friday'] = reduceFloat(friday());
+    // payload['sat'] = reduceFloat(sat());
+    // payload['sun'] = reduceFloat(sun());
 
     return payload;
 }
