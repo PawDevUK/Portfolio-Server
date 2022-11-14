0d7779e2ec6d5c76ddcc5fd1c024912f8c7b76b9
GitcrackerUK <pfsiwek@gmail.com>
2022-11-14 19:51:45 +0000

    removed check if calendar or start time is diff

--- routes/WorkTracker/factory/development.js
+++ routes/WorkTracker/factory/development.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const FY = require('../store/fullYearCalendar.json')
 
-
-
-function writeFullYear(payload, start_Time) {
-    if (FY.length === 0 || FY[0].calendar[0].start !== payload[0].calendar[0].start_Time) {
+function writeFullYear(payload) {
         const p = JSON.stringify(payload);
         fs.writeFile(`${__dirname}/../store/fullYearCalendar.json`, p, (err) => {
             if (err) {
