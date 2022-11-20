040874fc35ba912a42150f3e52a9851bd81ce6a9
GitcrackerUK <pfsiwek@gmail.com>
2022-11-20 23:33:16 +0000

    added user schema

---
+++ routes/WorkTracker/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    user:{
+        type:String,
+        require:true,
+        unique:true,
+        minLength:5,
+        maxLength:12
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        minLength:5,
+        maxLength:12
+    },
+    password:{
+        type:String,
+        require:true,
+        minLength:5,
+        maxLength:12
+    },
+    calendar:{
+        type:[]
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
