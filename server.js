f303b994390748aba70f7415c80ad1ca06222f61
GitcrackerUK <pfsiwek@gmail.com>
2022-11-30 13:22:50 +0000

    allowed all cors

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin:'http://localhost:3000'
-}));
+app.use(cors());
 
 const Port = process.env.PORT || 8080;
 
