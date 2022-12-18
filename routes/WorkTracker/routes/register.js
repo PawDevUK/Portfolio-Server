612dd6f73498c4ecff7cab89571d1e12b2b12245
GitcrackerUK <pfsiwek@gmail.com>
2022-12-18 22:10:47 +0000

    refactored

--- routes/WorkTracker/routes/register.js
+++ routes/WorkTracker/routes/register.js
@@ -1,36 +1,45 @@
 const User = require('../models/user.model.js')
 const router = require('express').Router()
 
-router.route('/').post((req,res)=>{
-    const {user, password, email} = req.body;
+router.route('/').post((req, res) => {
+  const { user, password, email } = req.body;
 
-    User.find({user:user}, (err, name)=>{
-        try{
-            if(name[0]){
-                res.send({message:'user already registered', user:name[0].user, calendar:name[0].calendar.length});
-            }else{
-                let newUser = new User({
-                    user,
-                    password,
-                    email,
-                    calendar:[]
-                    })
-                newUser.save((err)=>{
-                    if(err){
-                        res.send({error:err.name,message:err.message})
-                    }else{
-                            res.send({message:'User Registered'})
-                    }
-                })  
-            }
-        }catch(err){
-            console.log(err);
-            res.send({
-                route:'register',
-                error:err
-            })
-        }
+  User.findOne({ user: user }, (err, name) => {
+    if (err) {
+      console.log(err);
+      return res.send({
+        route: 'register',
+        error: err,
+      });
+    }
+
+    if (name) {
+      return res.send({
+        message: 'user already registered',
+        user: name.user,
+        calendar: name.calendar.length,
+      });
+    }
+
+    const newUser = new User({
+      user,
+      password,
+      email,
+      calendar: [],
     });
-})
+    
+    newUser.save((error) => {
+      if (error) {
+        return res.send({
+          error: error.name,
+          message: error.message,
+        });
+      }
+
+      return res.send({ message: 'User Registered' });
+    });
+  });
+});
+
 
 module.exports = router
