4ab46c2582fd5644dd16abd4376dc5758b880216
GitcrackerUK <pfsiwek@gmail.com>
2022-12-18 22:05:37 +0000

    added try and catch

--- routes/WorkTracker/routes/register.js
+++ routes/WorkTracker/routes/register.js
@@ -5,25 +5,30 @@ router.route('/').post((req,res)=>{
     const {user, password, email} = req.body;
 
     User.find({user:user}, (err, name)=>{
-        if(name[0]){
-            res.send({message:'user already registered', user:name[0].user, calendar:name[0].calendar.length});
-        }else{
-            let newUser = new User({
-                user,
-                password,
-                email,
-                calendar:[]
-                })
-            newUser.save((err)=>{
-                if(err){
-                    res.send({error:err.name,message:err.message})
-                }else{
-                        res.send({message:'User Registered'})
-                }
+        try{
+            if(name[0]){
+                res.send({message:'user already registered', user:name[0].user, calendar:name[0].calendar.length});
+            }else{
+                let newUser = new User({
+                    user,
+                    password,
+                    email,
+                    calendar:[]
+                    })
+                newUser.save((err)=>{
+                    if(err){
+                        res.send({error:err.name,message:err.message})
+                    }else{
+                            res.send({message:'User Registered'})
+                    }
+                })  
+            }
+        }catch(err){
+            console.log(err);
+            res.send({
+                route:'register',
+                error:err
             })
-            
-            
-            
         }
     });
 })
