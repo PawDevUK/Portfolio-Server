d2d207899938ea8b465892ab26b55435017bb0e1
GitcrackerUK <pfsiwek@gmail.com>
2023-01-19 14:14:41 +0000

    added console logs for DB instance.

--- DB.js
+++ DB.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 function DB(url, var_name){
+    console.log(`DB - url:${url}`);
+    console.log(`DB - var_name:${var_name}`);
     const connection =  mongoose.createConnection(url, {
         // (node:9612) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
         // removes deprecation 
