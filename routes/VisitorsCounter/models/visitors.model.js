bee42df03659f941d89a4af28b95ce8ebd7882f3
pawel <pfsiwek@gmail.com>
2023-04-15 00:46:32 +0100

    commented out calls to db as there is no .env

--- routes/VisitorsCounter/models/visitors.model.js
+++ routes/VisitorsCounter/models/visitors.model.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose')
 const DB = require('../../../DB')
 require('dotenv').config();
 
-const PORTFOLIO_URI = process.env.PORTFOLIO_URI;
-let varName = Object.keys({PORTFOLIO_URI})[0]
+// const PORTFOLIO_URI = process.env.PORTFOLIO_URI;
+// let varName = Object.keys({PORTFOLIO_URI})[0]
 
-const Schema = mongoose.Schema
+// const Schema = mongoose.Schema
 
-const visitorsSchema = new Schema({
-    counter: {
-        type: Number,
-        required: true
-    }
-}, {
-    timestamps: true
-})
+// const visitorsSchema = new Schema({
+//     counter: {
+//         type: Number,
+//         required: true
+//     }
+// }, {
+//     timestamps: true
+// })
 
-const visitor = DB(PORTFOLIO_URI, varName).model('Visitor', visitorsSchema);
+// const visitor = DB(PORTFOLIO_URI, varName).model('Visitor', visitorsSchema);
 
 module.exports = visitor
--- routes/WorkTracker/models/user.model.js
+++ routes/WorkTracker/models/user.model.js
@@ -3,34 +3,34 @@ const Schema = mongoose.Schema;
 const DB = require('../../../DB');
 require('dotenv').config();
 
-const TESCO_USERS_URI = process.env.TESCO_USERS_URI;
-let varName = Object.keys({TESCO_USERS_URI})[0]; // It is to pass name of the variable.
+// const TESCO_USERS_URI = process.env.TESCO_USERS_URI;
+// let varName = Object.keys({TESCO_USERS_URI})[0]; // It is to pass name of the variable.
 
-const userSchema = new Schema({
-    user:{
-        type:String,
-        require:true,
-        unique:true,
-        minLength:5,
-        maxLength:12
-    },
-    email:{
-        type:String,
-        require:true,
-        unique:true,
-        minLength:5,
-    },
-    password:{
-        type:String,
-        require:true,
-        minLength:5,
-        maxLength:12
-    },
-    calendar:{
-        type:[]
-    }
-})
+// const userSchema = new Schema({
+//     user:{
+//         type:String,
+//         require:true,
+//         unique:true,
+//         minLength:5,
+//         maxLength:12
+//     },
+//     email:{
+//         type:String,
+//         require:true,
+//         unique:true,
+//         minLength:5,
+//     },
+//     password:{
+//         type:String,
+//         require:true,
+//         minLength:5,
+//         maxLength:12
+//     },
+//     calendar:{
+//         type:[]
+//     }
+// })
 
-const User = DB(TESCO_USERS_URI,varName).model('User', userSchema);
+// const User = DB(TESCO_USERS_URI,varName).model('User', userSchema);
 
-module.exports = User
+// module.exports = User
